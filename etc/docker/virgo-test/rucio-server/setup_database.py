ae30c854a85cb1e816d2c3224b1044cdd115041f
Gabriele <sucre.91@hotmail.it>
2019-01-16 12:15:30 +0100

    Adding setup_database.py to create the database schema for replication

---
+++ etc/docker/virgo-test/rucio-server/setup_database.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Copyright 2012-2018 CERN for the benefit of the ATLAS collaboration.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Thomas Beermann, <thomas.beermann@cern.ch>, 2018
+# - Cedric Serfon, <cedric.serfon@cern.ch>, 2018
+
+import os
+
+from rucio.api.account import add_account
+from rucio.api.identity import add_account_identity
+from rucio.db.sqla.util import build_database, create_root_account
+
+if __name__ == '__main__':
+    # Create the Database and the root account
+    build_database()
+    create_root_account()
+    add_account_identity('/CN=docker client', 'x509', 'root', 'test@rucio.com', issuer="root")
