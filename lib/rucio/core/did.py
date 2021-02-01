da8bb456caddc3ee8eb3917d2540c8d05234b381
Gabriele <sucre.91@hotmail.it>
2021-02-01 13:26:48 +0100

    Fix

--- lib/rucio/core/did.py
+++ lib/rucio/core/did.py
@@ -1441,11 +1441,12 @@ def list_dids(scope, filters=None, type='collection', ignore_case=False, limit=N
                     rec_filters = filters.replace(";", ", name == %s;" % did['name'])
                 else:
                     splitted = filters.split(';')
+                    new_filters = []
                     for s in splitted:
                         tmp = [c if not "name" in c else "<put_name_here>" for c in s.split(',')]
                         print("Temp filters: ",tmp)
-                        rec_filters += (', '.join(tmp).replace("<put_name_here>", "name=={},".format(did['name']))).rstrip(',').replace('  ', ' ')+'; '
-                    rec_filters.rstrip(' ').rstrip(';')
+                        new_filters.append((', '.join(tmp).replace("<put_name_here>", "name=={},".format(did['name']))).rstrip(',').replace('  ', ' '))
+                    rec_filters = '; '.join(new_filters)
                 print("New filters: ",rec_filters)
                 for result in list_dids(scope=did['scope'], filters=rec_filters, recursive=True, type=type, limit=limit, offset=offset, long=long, session=session):
                     yield result
